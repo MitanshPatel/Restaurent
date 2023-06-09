9929f1847f3c9f1939660fffbf891de7d3627bdd
Mitansh Patel <mitanshpatel288@gmail.com>
2023-06-09 19:13:08 +0530

    using reducer

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ import {
 } from "react-router-dom";
 import Login from './screens/Login';
 import SignUp from './screens/SignUp'
+import { CartProvider } from './components/contextReducer';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Home />} />      {/*element to render is Home when path is "/" */}
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/createuser" element={<SignUp />} />
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />      {/*element to render is Home when path is "/" */}
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/createuser" element={<SignUp />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
